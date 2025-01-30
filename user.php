fad4ade8d473f554b1b8eff7078073d58db98d96
Aaron Maraya <aaronmaraya7@gmail.com>
2025-01-30 21:28:48 +0800

    create variables for user credentials.
    file:
    - user.php

--- user.php
+++ user.php
@@ -1,3 +1,14 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    include 'connect.php';
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS) ?? null;
+    $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL) ?? null;
+    $mobile = filter_input(INPUT_POST, 'mobile', FILTER_SANITIZE_NUMBER_INT) ?? null;
+    $password = $_POST['password'] ?? null;
+}
+?>
+
 <!doctype html>
 <html lang="en">
   <head>
