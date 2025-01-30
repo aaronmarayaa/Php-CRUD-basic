648194a7d7dfbc98c4d22ca288a28d8866dfe0df
Aaron Maraya <aaronmaraya7@gmail.com>
2025-01-30 15:51:30 +0800

    add more inputs for user information.
    file:
    - user.php

--- user.php
+++ user.php
@@ -8,13 +8,30 @@
     <title>Crud</title>
   </head>
   <body>
-    <main>
-        <form action="user.php" method="post ">
+    <main class="d-flex justify-content-center m-4">
+        <div class="card w-50 p-4">
+            <form action="user.php" method="post ">
+                <div class="mb-3">
+                <label for="name" class="form-label">Name: </label>
+                <input type="text" class="form-control" id="name" placeholder="Enter Name">
+            </div>
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                <label for="email" class="form-label">Email address: </label>
+                <input type="email" class="form-control" id="email" placeholder="Enter Email Address">
+            </div>
+            <div class="mb-3">
+                <label for="mobile" class="form-label">Mobile: </label>
+                <input type="number" class="form-control" id="mobile" placeholder="Enter Mobile">
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Password: </label>
+                <input type="password" class="form-control" id="password" placeholder="Enter Password">
+            </div>
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary w-50">Submit</button>
             </div>
         </form>
+    </div>
     </main>
   </body>
 </html>
