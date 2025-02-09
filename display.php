94d6e5a665591f2aec7d86315d02848a52250c6e
Aaron Maraya <aaronmaraya7@gmail.com>
2025-02-09 19:36:05 +0800

    First commit.
    file:
     - display.php

---
+++ display.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Home</title>
+  </head>
+  <body>
+    <main>
+        <div class="container m-3">
+            <button class="btn btn-primary"><a href="user.php" class="text-light text">Add user</a></button>
+        </div>
+    </main>
+  </body>
+</html>
